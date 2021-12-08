f10f6ced35238b917ccb1ea6eaf9452d5dd1c224
Lucas Lederman <ljlederman@gmail.com>
2021-12-08 17:07:59 +0000

    Continuing set up and drawing first lines

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let initDemo = () => {
+const initDemo = () => {
     console.log("This is working");
     //below fetches the element from the HTML and then attempts to initialize WEBGL, changes based on browser
     let canvas = document.getElementById("game-surface");
@@ -14,5 +14,15 @@ let initDemo = () => {
     };
 
     //this is (red, green, blue, alpha) alpha will be 1.0 meaning opaque, 0 is invisible
-    gl.clearColor(0.75, 0.85, 0.8, 1.0)
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    //this sets up the above changes
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 };
+
+const vertexShader = (vertPosition, vertColor) => {
+    //sets fragColor to whatever the vertex color was, and position sets where on the rendering canvas you want to draw that vertix
+    return {
+        fragColor: vertColor,
+        gl_position: [vertPosition.x, vertPosition.y, 0.0, 1.0]
+    };
+}
